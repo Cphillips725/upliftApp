33c3f4a3aff434ddfeac5cfd2809fa676625c9f3
Christopher <cphillips72590@gmail.com>
2021-04-28 10:53:57 -0500

    styling complete

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
-const QuoteBtn = document.getElementById('QuoteBtn');
-
-const QuoteText = document.getElementById('QuoteText');
-
- 
-
-QuoteBtn.addEventListener('click', () => {
-
+document.addEventListener('DOMContentLoaded', () => {
   console.log('clicked the button');
-
-  const url = 'https://api.kanye.rest'
-  
-  
-
-  fetch(url) 
-
-  .then((res) => {
-
-      return res.json();
-
+    document.getElementById("QuoteBtn").onclick = myFunction;
+   function myFunction(){
+    const url = 'https://api.kanye.rest'
+    fetch(url)
+    .then(resp => resp.json())
+    .then(data =>{
+    document.getElementById("QuoteText").innerHTML = data.quote;
+    })
+   }
   })
-
-.then((data) => {
-
-    console.log(data)
-
-    QuoteText.innerHTML = data.quote
-
-    Quotekanye.innerHTML = data.author
-
-  })
-
-});
-
- 
-
